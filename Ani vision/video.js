7866d2e7a4dbcf3d7a0cb2201dc403ce4e361e41
anivision090 <seemahaider1430909@gmail.com>
2026-01-06 15:20:24 +0530

    Create video.js

---
+++ Ani vision/video.js
@@ -0,0 +1,124 @@
+// All video links stored here - DO NOT MODIFY OTHER FILES
+const videoData = {
+    // All videos (45 videos)
+    "all": [
+        {id: 1, title: "Anime Action 1", url: "https://drive.google.com/file/d/1-NqVX5qVomEpWILFBDNBR2cq2sjliy1C/preview", category: "all"},
+        {id: 2, title: "AMV Epic Battle", url: "https://drive.google.com/file/d/12AZCkUhNcgYNRwtId75MRKkiFtchfwsw/preview", category: "basic"},
+        {id: 3, title: "Romantic Moments", url: "https://drive.google.com/file/d/15vQxUtZykNRaCSN7R2OYMqS6kPP-Fg6y/preview", category: "basic"},
+        {id: 4, title: "Shonen Compilation", url: "https://drive.google.com/file/d/16QNB2RScSbPShXikA7VUKfiovDcR85_u/preview", category: "basic"},
+        {id: 5, title: "Opening Collection", url: "https://drive.google.com/file/d/17L8KgBgCTKf_GG2VyLh7yo-lKMJYW0jY/preview", category: "basic"},
+        {id: 6, title: "Best Fight Scenes", url: "https://drive.google.com/file/d/17wVuylTnIdLsYLe1w7cL6hWzNpFrdYkj/preview", category: "basic"},
+        {id: 7, title: "Sad Moments", url: "https://drive.google.com/file/d/184_EG90XqgRzm9rTZnoRl6afDzWuR8aU/preview", category: "basic"},
+        {id: 8, title: "Comedy Clips", url: "https://drive.google.com/file/d/18F1AIrxb1wVX2SLNP5kZx-9DfU5iU9Px/preview", category: "basic"},
+        {id: 9, title: "Isekai Montage", url: "https://drive.google.com/file/d/19oeRaJcGQtu3o6SouDyvJefd9zKHwPTH/preview", category: "basic"},
+        {id: 10, title: "Transformations", url: "https://drive.google.com/file/d/1Bgo6Och-fpM28pAuNssG8CmKTc6EowLx/preview", category: "basic"},
+        {id: 11, title: "Fantasy World", url: "https://drive.google.com/file/d/1Cn_fozUPz_HVmFHgmoHH8_kQuNURSHVI/preview", category: "basic"},
+        {id: 12, title: "Slice of Life", url: "https://drive.google.com/file/d/1Ec4K-wBzoAy5VbhfbrI2CKyJtmjcnpGP/preview", category: "basic"},
+        {id: 13, title: "Power Ups", url: "https://drive.google.com/file/d/1F3fVmMaQd_OwHRKyhPVQeWgFo6L9vTon/preview", category: "basic"},
+        {id: 14, title: "Mystery Compilation", url: "https://drive.google.com/file/d/1G3HW4DO1BR8b-Md6pTSGo1VczHlpkggE/preview", category: "basic"},
+        {id: 15, title: "School Life", url: "https://drive.google.com/file/d/1HLwDILy12YmU3VvC1f7XIkQTVeR_aodn/preview", category: "basic"},
+        {id: 16, title: "Heroic Moments", url: "https://drive.google.com/file/d/1HUQciHH3geeuoVKnMhG85jwkKBY4qpJa/preview", category: "basic"},
+        {id: 17, title: "Villain Compilation", url: "https://drive.google.com/file/d/1HomYt0iUV0EA_LQKavwz96QKP2uc8Iec/preview", category: "basic"},
+        {id: 18, title: "OST Visuals", url: "https://drive.google.com/file/d/1MVCuw6jVmUvn0SLNW01ciOLU6HNS5ZSD/preview", category: "basic"},
+        {id: 19, title: "Supernatural Scenes", url: "https://drive.google.com/file/d/1Orlh23S643G0VSY0XFSpCjw_JPxJi_F4/preview", category: "basic"},
+        {id: 20, title: "Travel Montage", url: "https://drive.google.com/file/d/1Q9Vzp5Sh5DrkXldRyPB25xGL7lRD3elD/preview", category: "basic"},
+        {id: 21, title: "Training Arc", url: "https://drive.google.com/file/d/1Qs7TxKlDpdf702T8if3ad_jcoLmgRnoi/preview", category: "basic"},
+        {id: 22, title: "Tournament Fights", url: "https://drive.google.com/file/d/1RysiEDW71B10RzSkbwzahYfXDAItY_Zx/preview", category: "basic"},
+        {id: 23, title: "Standard Edit 1", url: "https://drive.google.com/file/d/1SuQCl9TAbo12Aq0yZoZHfC1gVzA-R5u6/preview", category: "standard"},
+        {id: 24, title: "Nostalgic Moments", url: "https://drive.google.com/file/d/1TI2wS_fPmZ9cvxle4sEnVFxGwRdpYpEv/preview", category: "basic"},
+        {id: 25, title: "Friendship Montage", url: "https://drive.google.com/file/d/1U8dmF4NpYCJ_RZMiERbaAVJ3zGjQrI0i/preview", category: "basic"},
+        {id: 26, title: "Adventure Compilation", url: "https://drive.google.com/file/d/1W5G5LHkVHyfziPx0AsXjofXsHsaKwNHM/preview", category: "basic"},
+        {id: 27, title: "Magic Spells", url: "https://drive.google.com/file/d/1Y7q3_GqHhjT_lCMqGB_LAqHNDTBobqOe/preview", category: "basic"},
+        {id: 28, title: "Mecha Highlights", url: "https://drive.google.com/file/d/1Z3-yST6FovSlU6GYR4J0-cWBfHq0oNCZ/preview", category: "basic"},
+        {id: 29, title: "Nature Scenes", url: "https://drive.google.com/file/d/1bC8qY2SDkEEoescgyRW6SPdWwBPjBaQj/preview", category: "basic"},
+        {id: 30, title: "Standard Edit 2", url: "https://drive.google.com/file/d/1c-S84k8naqecRhl25T5WJzk30VN2bk_3/preview", category: "standard"},
+        {id: 31, title: "City Life", url: "https://drive.google.com/file/d/1cOV2o4mE1_Tl24sTetotyGF9g6zy2ZQp/preview", category: "basic"},
+        {id: 32, title: "Sports Highlights", url: "https://drive.google.com/file/d/1hogxCFFgOM77Z-5p2ZoaVo_5NN69me9Y/preview", category: "basic"},
+        {id: 33, title: "Winter Scenes", url: "https://drive.google.com/file/d/1i4NYuv0khHlOAIHnuxWNic3WNf6vXkBR/preview", category: "basic"},
+        {id: 34, title: "Standard Edit 3", url: "https://drive.google.com/file/d/1jIeVoua8fCRehmDWSteWlvkbwHl5rZQT/preview", category: "standard"},
+        {id: 35, title: "Beach Episodes", url: "https://drive.google.com/file/d/1k9mwzbOIrJIdzVrxGdsJ-Bx36Df1kFhA/preview", category: "basic"},
+        {id: 36, title: "Nighttime Scenes", url: "https://drive.google.com/file/d/1kj7FRgu_d1ly492lDr34gu892LhqVKoa/preview", category: "basic"},
+        {id: 37, title: "Festival Montage", url: "https://drive.google.com/file/d/1oDxPBLa1lUKGz5ElUdlxdQbGOJKg39dY/preview", category: "basic"},
+        {id: 38, title: "HQ Edit 1", url: "https://drive.google.com/file/d/1r4vOuy23_xg5Uxa5rubGt1aA0B8c6USg/preview", category: "high-quality"},
+        {id: 39, title: "Food Scenes", url: "https://drive.google.com/file/d/1rIh91a5nIfYMGRXCS98pr6Axas4gj9d9/preview", category: "basic"},
+        {id: 40, title: "Pets Compilation", url: "https://drive.google.com/file/d/1sBFsNHsm_WCvMFOxlZ1H88Z7p6tGRvVI/preview", category: "basic"},
+        {id: 41, title: "HQ Edit 2", url: "https://drive.google.com/file/d/1wI2bU9o1x3RJh2CprMrm_MNW3IBjviuh/preview", category: "high-quality"},
+        {id: 42, title: "Vehicle Scenes", url: "https://drive.google.com/file/d/1xWAfPA-BhhrNislE2mM1fNuHlK6_q1gd/preview", category: "basic"},
+        {id: 43, title: "Standard Edit 4", url: "https://drive.google.com/file/d/1yfQn5VQBTtbrDqcS0QtmsQsJuR8qabOa/preview", category: "standard"},
+        {id: 44, title: "Wedding Moments", url: "https://drive.google.com/file/d/1z8FCBqQLuYvJg9xBbEFUNbD13eFBKdgJ/preview", category: "basic"},
+        {id: 45, title: "Final Battle", url: "https://drive.google.com/file/d/1vkk4Q4-AYQAymc1r6qpQx4MB_4UDloSz/preview", category: "basic"}
+    ],
+    
+    // Basic Edits
+    "basic": [
+        {id: 2, title: "AMV Epic Battle", url: "https://drive.google.com/file/d/12AZCkUhNcgYNRwtId75MRKkiFtchfwsw/preview", category: "basic"},
+        {id: 3, title: "Romantic Moments", url: "https://drive.google.com/file/d/15vQxUtZykNRaCSN7R2OYMqS6kPP-Fg6y/preview", category: "basic"},
+        {id: 4, title: "Shonen Compilation", url: "https://drive.google.com/file/d/16QNB2RScSbPShXikA7VUKfiovDcR85_u/preview", category: "basic"},
+        {id: 5, title: "Opening Collection", url: "https://drive.google.com/file/d/17L8KgBgCTKf_GG2VyLh7yo-lKMJYW0jY/preview", category: "basic"},
+        {id: 6, title: "Best Fight Scenes", url: "https://drive.google.com/file/d/17wVuylTnIdLsYLe1w7cL6hWzNpFrdYkj/preview", category: "basic"},
+        {id: 7, title: "Sad Moments", url: "https://drive.google.com/file/d/184_EG90XqgRzm9rTZnoRl6afDzWuR8aU/preview", category: "basic"},
+        {id: 8, title: "Comedy Clips", url: "https://drive.google.com/file/d/18F1AIrxb1wVX2SLNP5kZx-9DfU5iU9Px/preview", category: "basic"},
+        {id: 9, title: "Isekai Montage", url: "https://drive.google.com/file/d/19oeRaJcGQtu3o6SouDyvJefd9zKHwPTH/preview", category: "basic"},
+        {id: 10, title: "Transformations", url: "https://drive.google.com/file/d/1Bgo6Och-fpM28pAuNssG8CmKTc6EowLx/preview", category: "basic"},
+        {id: 11, title: "Fantasy World", url: "https://drive.google.com/file/d/1Cn_fozUPz_HVmFHgmoHH8_kQuNURSHVI/preview", category: "basic"},
+        {id: 12, title: "Slice of Life", url: "https://drive.google.com/file/d/1Ec4K-wBzoAy5VbhfbrI2CKyJtmjcnpGP/preview", category: "basic"},
+        {id: 13, title: "Power Ups", url: "https://drive.google.com/file/d/1F3fVmMaQd_OwHRKyhPVQeWgFo6L9vTon/preview", category: "basic"},
+        {id: 14, title: "Mystery Compilation", url: "https://drive.google.com/file/d/1G3HW4DO1BR8b-Md6pTSGo1VczHlpkggE/preview", category: "basic"},
+        {id: 15, title: "School Life", url: "https://drive.google.com/file/d/1HLwDILy12YmU3VvC1f7XIkQTVeR_aodn/preview", category: "basic"},
+        {id: 16, title: "Heroic Moments", url: "https://drive.google.com/file/d/1HUQciHH3geeuoVKnMhG85jwkKBY4qpJa/preview", category: "basic"},
+        {id: 17, title: "Villain Compilation", url: "https://drive.google.com/file/d/1HomYt0iUV0EA_LQKavwz96QKP2uc8Iec/preview", category: "basic"},
+        {id: 18, title: "OST Visuals", url: "https://drive.google.com/file/d/1MVCuw6jVmUvn0SLNW01ciOLU6HNS5ZSD/preview", category: "basic"},
+        {id: 19, title: "Supernatural Scenes", url: "https://drive.google.com/file/d/1Orlh23S643G0VSY0XFSpCjw_JPxJi_F4/preview", category: "basic"},
+        {id: 20, title: "Travel Montage", url: "https://drive.google.com/file/d/1Q9Vzp5Sh5DrkXldRyPB25xGL7lRD3elD/preview", category: "basic"},
+        {id: 21, title: "Training Arc", url: "https://drive.google.com/file/d/1Qs7TxKlDpdf702T8if3ad_jcoLmgRnoi/preview", category: "basic"},
+        {id: 22, title: "Tournament Fights", url: "https://drive.google.com/file/d/1RysiEDW71B10RzSkbwzahYfXDAItY_Zx/preview", category: "basic"},
+        {id: 24, title: "Nostalgic Moments", url: "https://drive.google.com/file/d/1TI2wS_fPmZ9cvxle4sEnVFxGwRdpYpEv/preview", category: "basic"},
+        {id: 25, title: "Friendship Montage", url: "https://drive.google.com/file/d/1U8dmF4NpYCJ_RZMiERbaAVJ3zGjQrI0i/preview", category: "basic"},
+        {id: 26, title: "Adventure Compilation", url: "https://drive.google.com/file/d/1W5G5LHkVHyfziPx0AsXjofXsHsaKwNHM/preview", category: "basic"},
+        {id: 27, title: "Magic Spells", url: "https://drive.google.com/file/d/1Y7q3_GqHhjT_lCMqGB_LAqHNDTBobqOe/preview", category: "basic"},
+        {id: 28, title: "Mecha Highlights", url: "https://drive.google.com/file/d/1Z3-yST6FovSlU6GYR4J0-cWBfHq0oNCZ/preview", category: "basic"},
+        {id: 29, title: "Nature Scenes", url: "https://drive.google.com/file/d/1bC8qY2SDkEEoescgyRW6SPdWwBPjBaQj/preview", category: "basic"},
+        {id: 31, title: "City Life", url: "https://drive.google.com/file/d/1cOV2o4mE1_Tl24sTetotyGF9g6zy2ZQp/preview", category: "basic"},
+        {id: 32, title: "Sports Highlights", url: "https://drive.google.com/file/d/1hogxCFFgOM77Z-5p2ZoaVo_5NN69me9Y/preview", category: "basic"},
+        {id: 33, title: "Winter Scenes", url: "https://drive.google.com/file/d/1i4NYuv0khHlOAIHnuxWNic3WNf6vXkBR/preview", category: "basic"},
+        {id: 35, title: "Beach Episodes", url: "https://drive.google.com/file/d/1k9mwzbOIrJIdzVrxGdsJ-Bx36Df1kFhA/preview", category: "basic"},
+        {id: 36, title: "Nighttime Scenes", url: "https://drive.google.com/file/d/1kj7FRgu_d1ly492lDr34gu892LhqVKoa/preview", category: "basic"},
+        {id: 37, title: "Festival Montage", url: "https://drive.google.com/file/d/1oDxPBLa1lUKGz5ElUdlxdQbGOJKg39dY/preview", category: "basic"},
+        {id: 39, title: "Food Scenes", url: "https://drive.google.com/file/d/1rIh91a5nIfYMGRXCS98pr6Axas4gj9d9/preview", category: "basic"},
+        {id: 40, title: "Pets Compilation", url: "https://drive.google.com/file/d/1sBFsNHsm_WCvMFOxlZ1H88Z7p6tGRvVI/preview", category: "basic"},
+        {id: 42, title: "Vehicle Scenes", url: "https://drive.google.com/file/d/1xWAfPA-BhhrNislE2mM1fNuHlK6_q1gd/preview", category: "basic"},
+        {id: 44, title: "Wedding Moments", url: "https://drive.google.com/file/d/1z8FCBqQLuYvJg9xBbEFUNbD13eFBKdgJ/preview", category: "basic"},
+        {id: 45, title: "Final Battle", url: "https://drive.google.com/file/d/1vkk4Q4-AYQAymc1r6qpQx4MB_4UDloSz/preview", category: "basic"}
+    ],
+    
+    // Standard Edits
+    "standard": [
+        {id: 23, title: "Standard Edit 1", url: "https://drive.google.com/file/d/1SuQCl9TAbo12Aq0yZoZHfC1gVzA-R5u6/preview", category: "standard"},
+        {id: 30, title: "Standard Edit 2", url: "https://drive.google.com/file/d/1c-S84k8naqecRhl25T5WJzk30VN2bk_3/preview", category: "standard"},
+        {id: 34, title: "Standard Edit 3", url: "https://drive.google.com/file/d/1jIeVoua8fCRehmDWSteWlvkbwHl5rZQT/preview", category: "standard"},
+        {id: 43, title: "Standard Edit 4", url: "https://drive.google.com/file/d/1yfQn5VQBTtbrDqcS0QtmsQsJuR8qabOa/preview", category: "standard"},
+        {id: 1, title: "Anime Action 1", url: "https://drive.google.com/file/d/1-NqVX5qVomEpWILFBDNBR2cq2sjliy1C/preview", category: "all"}
+    ],
+    
+    // High Quality Edits
+    "high-quality": [
+        {id: 38, title: "HQ Edit 1", url: "https://drive.google.com/file/d/1r4vOuy23_xg5Uxa5rubGt1aA0B8c6USg/preview", category: "high-quality"},
+        {id: 41, title: "HQ Edit 2", url: "https://drive.google.com/file/d/1wI2bU9o1x3RJh2CprMrm_MNW3IBjviuh/preview", category: "high-quality"}
+    ],
+    
+    // Best of Ani Vision (Empty)
+    "best": []
+};
+
+// Category display names
+const categoryNames = {
+    "all": "All Edits",
+    "basic": "Basic Edits",
+    "standard": "Standard Edits",
+    "high-quality": "High Quality Edits",
+    "best": "Best of Ani Vision",
+    "home": "Home",
+    "history": "History"
+};
+
+console.log("✅ Video.js loaded successfully!");
